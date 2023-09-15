d9e7a03abdde56c8ccebd756e73994ed162a807e
[200~ <luis.aprendas26@gmail.com>
2023-09-15 12:50:38 -0600

    Se agrega proyecto

---
+++ C#/#Estructura de control simple.py
@@ -0,0 +1,21 @@
+#Estructura de control simple
+a = 10
+b = 38  
+if (a>b): 
+    print ("a es mayor")
+elif (a<b):
+    print ("b es mayor")
+else: 
+    print("a y b son iguales")
+
+# Estructura de control simple con el metodo input()
+
+print()
+
+
+#Estructura de control iterativa
+
+contador= 1
+while (contador <= 10):
+    print("Paso:", str(contador))
+    contador += 1
